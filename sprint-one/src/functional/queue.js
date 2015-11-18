173920438327c2e046def8291b969164b4929522
radelmann <robadelmann@gmail.com>
2015-11-18 14:56:10 -0800

    Finished stack and queue procedural

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,28 @@ var Queue = function() {
   var storage = {};
 
   // Implement the methods below
-
+  var size = 0;
   someInstance.enqueue = function(value) {
+    size+=1;
+    storage[size] = value;
   };
 
   someInstance.dequeue = function() {
+    if(size > 0){
+      var value = storage[1];
+      for(var key in storage){
+      storage[key-1] = storage[key];
+      }
+      delete storage[size];
+      size--;
+      return value;
+    }
+    
+    return 
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    size+=1;
+    storage[size] = value;
   };
 
   someInstance.pop = function() {
+    if (size > 0) {
+      var value = storage[size];
+      delete storage[size];  
+      size-=1;
+      return value;
+    } 
+    return;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
