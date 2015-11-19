1e280517d5595f2083fbde394e480a51cc6471be
radelmann <robadelmann@gmail.com>
2015-11-19 15:35:15 -0800

    Completed test cases for hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -2,19 +2,25 @@
 
 var HashTable = function() {
   this._limit = 8;
-  this._storage = LimitedArray(this._limit);
+  this._storage = LimitedArray(this._limit); //Returns a empty object with limit
 };
 
+HashTable.prototype.getIndexBelowMaxForKey = getIndexBelowMaxForKey;
+
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this.getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this.getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
+
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this.getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
