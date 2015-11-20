35c4d2cc5108910868f3b25da9cdd400992e5b91
radelmann <robadelmann@gmail.com>
2015-11-19 20:26:34 -0800

    Fixed hashtable to handle function collisions properly

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,32 +1,34 @@
 
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit); //Returns a empty object with limit
 };
 
-HashTable.prototype.getIndexBelowMaxForKey = getIndexBelowMaxForKey;
-
 HashTable.prototype.insert = function(k, v) {
-  var index = this.getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, v);
+  if (this._count===this._limit) {
+    this._limit = this._limit*2;
+  }
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (typeof bucket === 'undefined') {
+    bucket = {};
+  }
+  bucket[k] = v;
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = this.getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index);
-
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  return bucket[k];
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = this.getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index, undefined);
-};
-
-
+  var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  delete bucket[k];
+ };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
