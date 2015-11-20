feae7fd25b25270344420394f6a9b3e973ec9954
radelmann <robadelmann@gmail.com>
2015-11-20 11:05:23 -0800

    Refractored Hashtable to use arrays instead of objects

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,34 +1,51 @@
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit); //Returns a empty object with limit
 };
 
 HashTable.prototype.insert = function(k, v) {
-  if (this._count===this._limit) {
-    this._limit = this._limit*2;
+  if (this._count === this._limit) {
+    this._limit = this._limit * 2;
   }
-  
+
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
   var bucket = this._storage.get(index);
-  bucket = bucket || {};
-  bucket[k] = v;
+  bucket = bucket || [];
+  bucket.push([k,v]);
   this._storage.set(index, bucket);
-  this._count++;
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
   var bucket = this._storage.get(index);
-  return bucket[k];
+  bucket = bucket || [];
+  var value;
+
+  _.each(bucket, function(item) {
+    if (item[0]===k) {
+      value = item[1];
+    }
+  });
+  
+  return value;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
   var bucket = this._storage.get(index);
-  delete bucket[k];
-  this._count--;
- };
+  var deleted;
+
+  _.each(bucket, function(item,index,collection) {
+    if (item[0]===k) {
+       deleted = collection.splice(index,1);
+    }
+  });
+  
+  return deleted;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
