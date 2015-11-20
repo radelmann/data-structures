98d82301860bb09886457380e960bdbdc50b1329
John Doe <jphuangjr@gmail.com>
2015-11-20 12:27:53 -0800

    Completed hash resize functionality

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,50 +1,77 @@
 var HashTable = function() {
+  this._count = 0;
   this._limit = 8;
   this._storage = LimitedArray(this._limit); //Returns a empty object with limit
 };
 
-HashTable.prototype.insert = function(k, v) {
-  if (this._count === this._limit) {
-    this._limit = this._limit * 2;
+HashTable.prototype.checkResize = function() {
+  if (this._count / this._limit > .75) {
+    this.resize(this._limit * 2);
+  } else if (this._count / this._limit < .25) {
+    this.resize(this._limit / 2);
   }
+}
+
+HashTable.prototype.resize = function(newLimit) {
+  var oldStorage = this._storage;
+  var ht = this;
+  this._storage = LimitedArray(newLimit);
+  this._limit = newLimit;
+  this._count = 0;
+  oldStorage.each(function(bucket) {
+    _.each(bucket, function(tuple) {
+      ht.insert.call(ht, tuple[0], tuple[1], false);
+    });
+  });
+}
+
+HashTable.prototype.insert = function(k, v, check) {
+  this._count++;
+  check = (check === undefined) ? true : false;
 
   var index = getIndexBelowMaxForKey(k, this._limit);
-  
+
   var bucket = this._storage.get(index);
   bucket = bucket || [];
-  bucket.push([k,v]);
+  bucket.push([k, v]);
   this._storage.set(index, bucket);
-  
+
+  if (check) {
+    this.checkResize();
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  
+
   var bucket = this._storage.get(index);
   bucket = bucket || [];
   var value;
 
   _.each(bucket, function(tuple) {
-    if (tuple[0]===k) {
+    if (tuple[0] === k) {
       value = tuple[1];
     }
   });
-  
+
   return value;
 };
 
 HashTable.prototype.remove = function(k) {
+  this._count--;
+
   var index = getIndexBelowMaxForKey(k, this._limit);
-  
   var bucket = this._storage.get(index);
   var deleted;
 
-  _.each(bucket, function(tuple,index,collection) {
-    if (tuple[0]===k) {
-       deleted = collection.splice(index,1);
+  _.each(bucket, function(tuple, index, collection) {
+    if (tuple[0] === k) {
+      deleted = collection.splice(index, 1);
     }
   });
-  
+
+  // check resize
+  this.checkResize();
   return deleted;
 };
 
