b5644c775ed1e0a86862711e928341045d80eb4a
radelmann <robadelmann@gmail.com>
2015-11-21 16:36:13 -0800

    Worked on binarySearchTree rebalancing

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -4,12 +4,63 @@ var BinarySearchTree = function(value) {
   tree.value = value;
   tree.left = null;
   tree.right = null;
+  tree.nodeCount = 1;
 
   _.extend(tree, bTreeMethods);
   return tree;
 };
 
 bTreeMethods = {};
+
+bTreeMethods.checkBalance = function() {
+  //rebalance as soon as the max depth is 
+  //more than twice the minimum depth
+  var minDepth = this.calcMinDepth();
+
+  if (this.nodeCount > 2 * minDepth) {
+    //rebalance
+    var array = [];
+    var func = function(value) {
+      array.push(value);
+    };
+    this.breadthFirstLog(func);
+
+    var mid = Math.floor(array.length / 2);
+    var newTree = BinarySearchTree(mid)
+
+    var i = 1;
+    
+    while (newTree.nodeCount < this.nodeCount) {
+      if (array[mid - i] !== null) newTree.insert(array[mid - i]);
+      if (array[mid + i] !== null) newTree.insert(array[mid + i]);
+      i++;
+    }
+    this = newTree;
+  }
+}
+
+bTreeMethods.calcMinDepth = function() {
+
+  var minDepth = function(node) {
+    if (node === null) return 0;
+
+    if (node.left === null && node.right === null) {
+      return 1;
+    }
+
+    if (!node.left) {
+      return minDepth(node.right) + 1;
+    }
+
+    if (!node.right) {
+      return minDepth(node.left) + 1;
+    }
+
+    return Math.min(minDepth(node.left), minDepth(node.right));
+  }
+
+  return minDepth(this);
+}
 
 bTreeMethods.insert = function(value) {
   if (value < this.value) {
@@ -25,6 +76,7 @@ bTreeMethods.insert = function(value) {
       this.right.insert(value);
     }
   }
+  this.nodeCount + 1;
 }
 
 bTreeMethods.contains = function(value) {
@@ -68,7 +120,7 @@ bTreeMethods.breadthFirstLog = function(cb) {
       }
     }
 
-    _.each(queue, function(obj){
+    _.each(queue, function(obj) {
       cb(obj.value);
     });
   }
