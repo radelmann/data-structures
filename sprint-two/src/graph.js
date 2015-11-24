609b523e19ec7f2f0f118745621a76dd1ef39759
radelmann <robadelmann@gmail.com>
2015-11-23 21:05:30 -0800

    Refactored graph to use objects instead of arrays

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,92 +1,56 @@
-
-
 // ------------------------
 // Instantiate a new graph
 var Graph = function() {
-  //need array for connections ?
-  //if only one connection it is an edge
-  //need to hold value
-  this.nodes = []; 
-
+  this.nodes = {};
 };
 
 // ------------------------
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
   var n = {};
-  n.edges = [];
-  n.value = node; 
-  this.nodes.push(n);
+  n.edges = {};
+  n.value = node;
+  this.nodes[node] = n;
 }
 
 // ------------------------
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  return _.reduce(this.nodes,function(found, currentNode) {
-    if (!found) {
-      if (currentNode.value === node) {
-        found = true;
-      } 
-      return found;
-    }
-  },false);
+  return (node in this.nodes);
 };
 
 // ------------------------
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  var nodes = this.nodes;
-  _.each(this.nodes, function(obj, key, collection) {
-    if (node === obj.value) {
-      collection.splice(key,1);
-      nodes = collection.slice();
-    };
-  });
-  this.nodes = nodes;
+  var value = this.nodes[node];
+  delete this.nodes[node];
+  return value;
 };
 
 // ------------------------
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  var found = false;
-  _.each(this.nodes, function(obj) {
-    if (fromNode === obj.value) {
-      found = _.contains(obj.edges, toNode);
-    }
-  });
-  return found;
+  return (toNode in this.nodes[fromNode].edges);
 };
 
 // ------------------------
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  _.each(this.nodes, function(obj) {
-    if (fromNode === obj.value) {
-      obj.edges.push(toNode);
-    }
-    if (toNode === obj.value) {
-      obj.edges.push(fromNode);
-    }
-  });
+  this.nodes[fromNode].edges[toNode] = toNode;
+  this.nodes[toNode].edges[fromNode] = fromNode;
 };
 
 // ------------------------
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  _.each(this.nodes, function(obj,index) {
-    if (fromNode === obj.value) {
-      obj.edges.splice(_.indexOf(obj.edges, toNode),1);
-    }
-    if (toNode === obj.value) {
-      obj.edges.splice(_.indexOf(obj.edges, fromNode),1);
-    }
-  });
+  delete this.nodes[fromNode].edges[toNode];
+  delete this.nodes[toNode].edges[fromNode];
 };
 
 // ------------------------
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-  _.each(this.nodes, function(obj){
+  _.each(this.nodes, function(obj) {
     cb(obj.value);
   })
 };
@@ -94,5 +58,3 @@ Graph.prototype.forEachNode = function(cb) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
